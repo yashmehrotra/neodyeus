17e76074e907e6723c0f79ce45e7b8d1044196ae
Yash Mehrotra <yashmehrotra95@gmail.com>
2018-06-30 12:20:09 +0530

    Docs

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+//    "bytes"
+    "os"
+    "github.com/ncw/rclone/fs/operations"
+    "github.com/ncw/rclone/cmd"
+_    "github.com/ncw/rclone/backend/s3"
+)
+
+/*
+type Manager interface {
+    Copy func(src string, dst string)
+    List func(src string)
+    Move func(src string, dst string)
+}
+*/
+
+
+func List(path []string) {
+    fsrc := cmd.NewFsSrc(path)
+    operations.List(fsrc, os.Stdout)
+}
+
+func main() {
+    path := []string{"spaces:dagobah"}
+    List(path)
+}
