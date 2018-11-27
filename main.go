1d75e86d1d1de905621120c7ae82e325783c109c
Yash Mehrotra <yashmehrotra95@gmail.com>
2018-11-27 12:11:06 +0530

    Content Type support

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/yashmehrotra/neodyeus/api/v1/fs"
+	//	"github.com/yashmehrotra/neodyeus/api/v1/user"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
@@ -26,11 +27,17 @@ func main() {
 	r := gin.Default()
 	r.Use(CORSMiddleware())
 
+	// File System routes
 	r.GET("/api/v1/list", fs.List)
 	r.PUT("/api/v1/mkdir", fs.Mkdir)
 	r.DELETE("/api/v1/remove", fs.Remove)
 	r.PUT("/api/v1/put", fs.Put)
 	r.GET("/api/v1/get", fs.Get)
 	r.GET("/api/v1/preview/*path", fs.Preview)
+
+	// User API routes
+	//	r.PUT("/api/v1/user/create", user.Create)
+	//	r.POST("/api/v1/user/auth", user.Auth)
+
 	r.Run()
 }
